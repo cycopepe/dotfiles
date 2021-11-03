3055f0a7b981343c076bb8d9dbeff21a6ad3ede1
cycopepe <cycopepe@gmail.com>
2021-11-02 21:03:40 -0500

    Create win-start.sh

---
+++ win-start.sh
@@ -0,0 +1,15 @@
+choco install notepadplusplus -y
+choco install kubernetes-cli -y
+#kubectl version
+#kubectl version --version
+#kubectl version --client
+choco install octant --confirm
+choco install kubectx
+choco install kubectx -y
+choco install minikube
+choco install kubernetes-helm -y
+choco install fluxctl -y
+choco install istioctl -y
+choco install dbeaver -y
+choco install postgresql -y
+choco install lens -y
